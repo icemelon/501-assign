73873e6cb2b093dcb931c2752310dff43d633bfb
Haichen Shen <shenhaichen@gmail.com>
2013-05-12 14:04:20 -0700

    add examples

---
+++ ssa/examples/test.dart
@@ -0,0 +1,41 @@
+void swap(int x, int y) {
+	int t;
+	int i;
+	i = 0;
+	while (i < 10) {
+		t = x;
+		x = y;
+		y = t;
+	}
+}
+
+void test(int x) {
+	int a, b, c, d;
+	int y, t;
+	a = 3;
+	b = 7;
+	c = 4;
+	d = 7;
+	t = 5;
+	if (t < 10) {
+		y = a + b;
+	} else {
+		y = c + d;
+	}
+}
+
+void foo() {
+}
+
+void main() {
+	int i;
+	int a, b;
+	i = 0;
+	while (i < 10) {
+		i = i + 1;
+	}
+	a = 1;
+	b = 2;
+	swap(a,b);
+}
+
