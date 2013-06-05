46077a812e570ae7b39242ce84663196a8a3c671
Haichen Shen <shenhaichen@gmail.com>
2013-06-05 09:16:17 -0700

    sync debug info

---
+++ runtest.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+def getCmdOutput(cmd):
+  child = os.popen(cmd)
+  data = child.read()
+  err = child.close()
+  if err:
+    raise(RuntimeError, '%s failed with exit code %d' %(cmd, err))
+  return data.split('\n')
+
+
+inputFile = "input/" + sys.argv[1] + ".start"
+firstRunCmd = "start -r -p " + inputFile
+os.system(firstRunCmd)
+optCmd = "./run.sh " + inputFile + " -profile=cfg -backend=asm > 1.start"
+os.system(optCmd)
+secondRunCmd = "start -r -p 1.start"
+os.system(secondRunCmd)
+
